ce537bbe0ad8e3ef48bdf71a9edac8d412221d9f
ShaotongChen <ccfxfx002@gmail.com>
2021-12-23 12:17:18 -0600

    first commit

---
+++ CMS_angular/cms-system-app/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'http://localhost:8080', //best practice to put your url in this environment file.//we can import this into our service and use it whenever we want
+};
